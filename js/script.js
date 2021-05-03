07e6adb35f1b31722dafec3da3b42fbad25e6f48
Zubi-Duru <duruazubuike2019@gmail.com>
2021-05-03 04:01:33 -0700

    debugged nav-toggle-btn

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "navDesign") {
-      x.className += " responsive";
+      // x.className += " responsive";
+      x.classList.add("active");
     } else {
-      x.className = "navDesign";
+      // x.className = "navDesign";
+      x.classList.remove("active")
     }
   };
 
